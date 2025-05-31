e8c5f613541ff4b4723dc729dee0f1a9007b59a9
hr-shuvo <h.r.shuvo9@gmail.com>
2025-05-31 10:21:47 +0600

    update seed data

--- src/Backend/Data/SeedData.cs
+++ src/Backend/Data/SeedData.cs
@@ -1,4 +1,5 @@
 using Backend.Models;
+using Microsoft.EntityFrameworkCore;
 using MongoDB.Driver;
 using MongoDB.Entities;
 
@@ -8,52 +9,91 @@ public class SeedData
 {
     public static async Task SeedProductsAsync(ApplicationDbContext context)
     {
-        if (!context.Products.Any())
+        try
         {
-            var products = new[]
+            await context.Database.MigrateAsync();
+
+            if (!context.Products.Any())
             {
+                var products = new[]
+                {
                 new Product
                 {
-                    Name = "Sample Product 1", Price = 100, DiscountStartDate = DateTime.UtcNow.AddDays(-1),
-                    DiscountEndDate = DateTime.UtcNow.AddDays(1), DiscountPercentage = 10
+                    Name = "DJI Phantom 2", Price = 500, DiscountStartDate = DateTime.UtcNow.AddDays(-1),
+                    DiscountEndDate = DateTime.UtcNow.AddDays(10), DiscountPercentage = 4
                 },
                 new Product
                 {
-                    Name = "Sample Product 2", Price = 200, DiscountStartDate = DateTime.UtcNow,
+                    Name = "DJI Phantom 4", Price = 1449, DiscountStartDate = DateTime.UtcNow,
+                    DiscountEndDate = DateTime.UtcNow.AddDays(20), DiscountPercentage = 5
+                },
+                new Product
+                {
+                    Name = "DJI Phantom 1 Vision", Price = 739, DiscountStartDate = DateTime.UtcNow,
+                    DiscountEndDate = DateTime.UtcNow.AddDays(2), DiscountPercentage = 5
+                },
+                new Product
+                {
+                    Name = "DJI Phantom 4 Pro", Price = 399, DiscountStartDate = DateTime.UtcNow,
+                    DiscountEndDate = DateTime.UtcNow.AddDays(20), DiscountPercentage = 12
+                },
+                new Product
+                {
+                    Name = "4 Series - Intelligent", Price = 98, DiscountStartDate = DateTime.UtcNow,
+                    DiscountEndDate = DateTime.UtcNow.AddDays(20), DiscountPercentage = 10
+                },
+                new Product
+                {
+                    Name = "4 Series - Amaging", Price = 739, DiscountStartDate = DateTime.UtcNow,
                     DiscountEndDate = DateTime.UtcNow.AddDays(2), DiscountPercentage = 20
                 }
             };
 
-            context.Products.AddRange(products);
-            await context.SaveChangesAsync();
+                context.Products.AddRange(products);
+                await context.SaveChangesAsync();
+            }
+
         }
+        catch (Exception)
+        {
+            throw new Exception("Failed to migrate/seed db, check db connection or check log");
+        }
+
     }
 
     public static async Task SeedCartsAsync(WebApplication app)
     {
-        var connectionString = app.Configuration.GetConnectionString("MongoDbConnection");
-        await DB.InitAsync("CartDb", MongoClientSettings.FromConnectionString(connectionString));
-        
-        var existingCarts = await DB.Find<Cart>().Limit(1).ExecuteAsync();
-        if (existingCarts.Count == 0)
+        try
         {
-            var cart1 = new Cart
-            {
-                // UserId = "",
-                Items =
-                [
-                    new CartItem { ProductId = 1, Quantity = 7 },
-                    new CartItem { ProductId = 2, Quantity = 3 }
-                ]
-            };
+            var connectionString = app.Configuration.GetConnectionString("MongoDbConnection");
+            await DB.InitAsync("CartDb", MongoClientSettings.FromConnectionString(connectionString));
 
-            var cart2 = new Cart
+            var existingCarts = await DB.Find<Cart>().Limit(1).ExecuteAsync();
+            if (existingCarts.Count == 0)
             {
-                Items = [new CartItem() { ProductId = 2, Quantity = 5 }]
-            };
+                var cart1 = new Cart
+                {
+                    // UserId = "",
+                    Items =
+                    [
+                        new CartItem { ProductId = 1, Quantity = 7 , Name = "DJI Phantom 2", Price=500},
+                        new CartItem { ProductId = 2, Quantity = 3 , Name="DJI Phantom 4", Price=1499},
+                    ]
+                };
 
-            await cart1.SaveAsync();
-            await cart2.SaveAsync();
+                var cart2 = new Cart
+                {
+                    Items = [new CartItem() { ProductId = 2, Quantity = 5 }]
+                };
+
+                await cart1.SaveAsync();
+                await cart2.SaveAsync();
+            }
         }
+        catch (Exception)
+        {
+            throw new Exception("Failed to migrate/seed mongo db, check db connection or check log");
+        }
+
     }
 }
